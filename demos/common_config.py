1ef17c4e7b367fdb051874aaeb2d7aea58c2b569
Binesh Bannerjee <binesh_binesh@hotmail.com>
2024-08-07 22:37:34 -0400

    Allow grab_config to find config information more flexibly.

--- demos/common_config.py
+++ demos/common_config.py
@@ -4,11 +4,22 @@
 """This is stuff that is common to all the bitcoin demos."""
 
 import os
+from functools import reduce
+
 import configparser
 
-def grab_config():
+def search_for_configfn():
+    candidates = [
+        os.path.join(os.path.realpath(path), fname)
+        for path in reversed([os.path.dirname(os.path.realpath(__file__))] + list(reduce(lambda acc, x: acc + [acc[-1] + "/" + x], os.getcwd().split("/")[1:], ["/"])) + ["/secrets"])
+        for fname in ("local.mk")
+    ]
+    candidates = [cand for cand in candidates if os.path.exists(cand)]
+    return candidates[0]
+
+def grab_config(configfn=None):
     config = configparser.ConfigParser()
-    localmkfn = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../local.mk"))
-    with open(localmkfn, "rt", encoding="utf-8") as cfp:
+    configfn = configfn or search_for_configfn()
+    with open(configfn, "rt", encoding="utf-8") as cfp:
         config.read_string(f"[DEFAULT]\n{cfp.read():s}\n")
     return config["DEFAULT"]
